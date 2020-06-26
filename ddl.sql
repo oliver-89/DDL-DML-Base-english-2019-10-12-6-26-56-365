f11c55e1d56516a5b6d28a362c5b7f0571054a4a
oliver-89 <wjr980729@163.com>
2020-06-26 21:28:52 +0800

    done

--- ddl.sql
+++ ddl.sql
@@ -1,31 +1,46 @@
 -- **Database level:**
 -- Display all the database
+SHOW DATABASES;
 
 -- Enter a certain database
+USE mysql;
 
 -- Create a database
+CREATE DATABASE student_examination_sys;
 
 -- Create the database of the designated character set
+CREATE DATABASE student_examination_sys DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 -- Display the creation information fo the database
+SHOW CREATE DATABASE student_examination_sys;
 
 -- Revise the codes of the database
+ALTER DATABASE ...
 
 -- Delete a database
+DROP DATABASE student_examination_sys;
 
 -- **Table level**
 -- Revise table name
+ALTER TABLE ... RENAME TO ...
 
 -- Revise the field's data type
+ALTER TABLE tablename MODIFY COLUMN columnname datatype
 
 -- Revise field name
+ALTER TABLE tablename CHANGE columnname newname datatype
 
 -- Add field
+ALTER TABLE tablename ADD columnname datatype
 
 -- Delete field
+ALTER TABLE tablename DROP columnname
 
 -- Revise the table's storage engine
+ALTER TABLE tablename ENGINE = ...
 
 -- Delete the table's foreign key restriant
+ALTER TABLE tablename DROP FOREIGN KEY foreignkeyname
 
 -- Delete a table
+DROP TABLE tablename
--- dml.sql
+++ dml.sql
@@ -1,7 +1,8 @@
 -- Insert record
-
+INSERT INTO student VALUES('003','Alice',20,'male');
 -- Revise record
-
+UPDATE student SET name = 'Nicki' WHERE id = '003'
 --  Delete record
-
+DELETE FROM student WHERE id = '003'
 -- Search record
+SELECT * FROM student WHERE id = '001'
